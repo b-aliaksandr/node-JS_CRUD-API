0537de043a06ecc4af32e9798fd93742c3f240d3
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-02-10 20:01:17 +0300

    feat: initialize users table

--- src/app.mjs
+++ src/app.mjs
@@ -7,9 +7,36 @@ import { httpError } from './utils.mjs';
 export async function build() {
   const memoryDB = await createMemoryDB({ logger: process.stdout });
   const router = await createRouter();
-  
-  usersRoutes({ router });
-  
+
+  try {
+    await memoryDB.createTable('users', [
+      {
+        name: 'id',
+        dataType: DATA_TYPES.STRING,
+        constraints: [CONSTRAINTS.REQUIRED, CONSTRAINTS.UNIQUE],
+      },
+      {
+        name: 'username',
+        dataType: DATA_TYPES.STRING,
+        constraints: [CONSTRAINTS.REQUIRED],
+      },
+      {
+        name: 'age',
+        dataType: DATA_TYPES.NUMBER,
+        constraints: [CONSTRAINTS.REQUIRED],
+      },
+      {
+        name: 'hobbies',
+        dataType: DATA_TYPES.ARRAY_OF_STRINGS,
+        constraints: [CONSTRAINTS.REQUIRED],
+      },
+    ]);
+  } catch (err) {
+    throw new Error(`memoryDB: ${err.message}`);
+  }
+
+  usersRoutes({ router, memoryDB });
+
   const server = http.createServer(async (req, res) => {
     const { url, method } = req;
     const route = router.routing({ method, url });
